c86c7e7343262a394a14322a5dc7491b394811c0
BlakeDelmar <blake.delmar3@gmail.com>
2023-11-30 17:35:09 -0800

    Changed how members class functions, added implementation of members class in program.cs, removed dictionarys from program.cs

--- ConsoleApp1/ConsoleApp1/Members.cs
+++ ConsoleApp1/ConsoleApp1/Members.cs
@@ -1,32 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public class Members
+    abstract class Members
     {
-        public int MemberId { get; set; }
+        public abstract string CheckIn();
+        
+    }
 
+    class MultiMember : Members
+    {
         public string MemberName { get; set; }
+        public int MemberId { get; set; }
+        public override string CheckIn()
+        {   
+            int membershipPoints = 0;
+            int membershipPointsAdded = membershipPoints + 1;
+            return membershipPointsAdded.ToString();
 
-        public Members(string memberName, int usersID)
+            //Need to merge to implement checking of Club
+            
+        }
+        public MultiMember(string memberName, int usersID)
         {
             MemberName = memberName;
+            MemberId = usersID;
+        }
+        
 
+    }
+    class SingleMember : Members
+    {
+        public string MemberName { get; set; }
+        public int MemberId { get; set; }
+        public override string CheckIn()
+        {
+            return string.Empty;
+
+            //Need to merge to implement checking of Club
+        }
+        public SingleMember(string memberName, int usersID)
+        {
+            MemberName = memberName;
             MemberId = usersID;
         }
 
-
     }
-     public class MemberType : Members
-     {
-
-
-        
-       
-     }
 
 }
