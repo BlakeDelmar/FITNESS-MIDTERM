f902e28f29d3dcb17a1a6c0c55f2798cead98943
Golden-Charm <annabananaperson@gmail.com>
2023-12-04 21:07:51 -0500

    Added functions and RemoveMember

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,45 +24,40 @@ namespace FitnessMidterm
 
 
             Console.WriteLine("Welcome to the Fitness center program!");
-            
+
             List<GymMember> membersList = new List<GymMember>();
-           
+
             string yesOrNo;
             while (true)
-            {   
-                
-                Console.WriteLine("Please enter member name");
-                string memberName = Console.ReadLine().Trim().ToUpper();
-                //ENTERING MEMBER NAME
-                
-                Console.WriteLine("Please enter member ID");
-                int usersID = DataValidator.GetValidationOfUserID();
-                //ENTERING MEMBER ID
+            {
+                string memberName = EnterMemberName();
+                int usersID = EnterMemberID();
+               
 
                 Console.WriteLine("What Club is the member apart of? You may pick from:");
                 Console.WriteLine("FitnessAreUs, ClubFitness, PlanetFitness, BallsOfSteel");
                 string ValidLocation1 = DataValidator.ValidLocation();
                 Console.WriteLine($"You picked {ValidLocation1}");
-                string address = string.Empty;               
+                string address = string.Empty;
                 if (ValidLocation1 == "FITNESSAREUS")
                 {
                     address = "FLORIDA 253 EASY AVE";
-                    
+
                 }
                 if (ValidLocation1 == "CLUBFITNESS")
                 {
                     address = "BRITISHCOLUMBIA 376 DONALD RD";
-                    
+
                 }
                 if (ValidLocation1 == "PLANETFITNESS")
                 {
                     address = "TEXAS 34 COLUMBIA DR";
-                    
+
                 }
                 if (ValidLocation1 == "BALLSOFSTEEL")
                 {
-                   address = "PHEONIX 68 RAMBO RD";
-                                      
+                    address = "PHEONIX 68 RAMBO RD";
+
                 }
                 Club club = new Club(ValidLocation1, address);
 
@@ -94,46 +89,79 @@ namespace FitnessMidterm
                     membersList.Add(member);
 
                 }
-                //ENTERING MEMBER TYPE
 
-                
-                Console.WriteLine("Would you like to view the member list? [Y/N]");
-                string yesOrNo3 = DataValidator.YesOrNo();
 
-                if (yesOrNo3 == "Y")
+                while (true)
                 {
-                    foreach (var listMember in membersList)
+                    Console.WriteLine("Would you like to view the member list? [Y/N]");
+                    string yesOrNo3 = DataValidator.YesOrNo();
+
+                    if (yesOrNo3 == "Y" || yesOrNo3 == "YES")
                     {
-                        Console.Write($"{listMember.Name} "); //IMPORTANT
-                        Console.Write($"{listMember.Id}\n");    
+                        foreach (var listMember in membersList)
+                        {
+                            Console.Write($"{listMember.Name} "); //IMPORTANT
+                            Console.Write($"{listMember.Id}\n");
+                        }
+                    }
+
+                    Console.WriteLine("Would you like to add another member? [Y/N]");
+                    string yesOrNo2 = DataValidator.YesOrNo();
+
+                    if (yesOrNo2 != "Y" || yesOrNo2 != "YES")
+                    {
+                        break;
                     }
                 }
-                
+                RemoveMember(membersList);
 
-                Console.WriteLine("Would you like to add another member? [Y/N]");
-                string yesOrNo2 = DataValidator.YesOrNo();
 
-                if (yesOrNo2 != "Y")
+                static string EnterMemberName()
                 {
-                    break;
+                    Console.WriteLine("Please enter member name");
+                    string memberName = Console.ReadLine().Trim().ToUpper();
+                    return memberName;
+                    //ENTERING MEMBER NAME
                 }
 
+                static int EnterMemberID()
+                {
+                    Console.WriteLine("Please enter member ID");
+                    int usersID = DataValidator.GetValidationOfUserID();
+                    return usersID;
+                    //ENTERING MEMBER ID
+                }
+
+
+                static void RemoveMember(List<GymMember> membersList)
+                {
+                    while (true)
+                    {
+                        //allow user to take a member off the list
+                        Console.WriteLine("Would you like to remove a member from the list? [Y/N]");
+                        string yesOrNo = DataValidator.YesOrNo();
+
+                        if (yesOrNo == "N" || yesOrNo == "NO")
+                        {
+                            break;
+                        }
+                        else if (yesOrNo == "YES" || yesOrNo == "Y")
+                        {
+                            Console.WriteLine("What is the name of the user you want to remove?");
+                            
+
+
+                        }
+                    }
+                    //static string CheckMemberClub()
+
+
+
+
+                }
+
+
             }
-
-            //int id;
-            //while (true)
-            //{
-            //    Console.WriteLine("Please enter a user ID to learn more about them [0-]");
-
-            //    string idInput = Console.ReadLine();
-
-            //    if (int.TryParse(idInput, out id))
-            //    {
-            //        break;
-            //    }
-            //    Console.WriteLine("Please enter valid id NUMBER");
-            //}
-                // NOT IMPLEMENTED
         }
     }
 }
